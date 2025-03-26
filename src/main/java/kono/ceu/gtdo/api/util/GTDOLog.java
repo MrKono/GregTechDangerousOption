85dc02a8e3f8bc0193220a9ce2b2d918a681e546
ko_no <90126004+MrKono@users.noreply.github.com>
2025-03-26 23:04:26 +0900

    Logも追加

---
+++ src/main/java/kono/ceu/gtdo/api/util/GTDOLog.java
@@ -0,0 +1,9 @@
+package kono.ceu.gtdo.api.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class GTDOLog {
+
+    public static Logger logger = LogManager.getLogger(GTDOValues.modName);
+}
