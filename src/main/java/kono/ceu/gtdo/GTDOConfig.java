b579e19329405e84105321de1503238362126c89
ko_no <90126004+MrKono@users.noreply.github.com>
2025-03-26 23:04:01 +0900

    cfgを追加

---
+++ src/main/java/kono/ceu/gtdo/GTDOConfig.java
@@ -0,0 +1,33 @@
+package kono.ceu.gtdo;
+
+import net.minecraftforge.common.config.Config;
+
+import kono.ceu.gtdo.api.util.GTDOValues;
+
+@Config(modid = GTDOValues.modId)
+public class GTDOConfig {
+
+    @Config.Name("Feature Setting")
+    @Config.Comment("Settings the features")
+    @Config.RequiresMcRestart
+    public static FeatureSetting features = new FeatureSetting();
+
+    @Config.Name("Explosion Setting")
+    @Config.Comment("Settings the item explosion")
+    @Config.RequiresMcRestart
+    public static ExplosionSetting explosion = new ExplosionSetting();
+
+    public static class FeatureSetting {
+
+        @Config.Comment({ "Whether specific material items explode when wet", "Default: true" })
+        public boolean explodeWhenWet = true;
+    }
+
+    public static class ExplosionSetting {
+
+        @Config.Comment({ "Material that explodes when wet", "Define material name and power with \":\"." })
+        public String[] wetExplosives = new String[] {
+                "lithium:1.0", "sodium:2.0", "potassium:4.0", "rubidium:8.0", "caesium:10.0", "francium:12.0"
+        };
+    }
+}
